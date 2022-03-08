11d08aabf690fbac535e81e21a98cad3d0312bd1
mjegan19 <87794144+mjegan19@users.noreply.github.com>
2022-03-08 15:32:04 +1100

    Select inputs change state and re-render data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,110 @@
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Components
+import Navbar from './components/Navbar';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table'
+import Container from 'react-bootstrap/Container';
+
+
 function App() {
+
+  // Set State
+  const [year, setYear] = useState(2021);
+  const [round, setRound] = useState(1);
+  const [matchData, setMatchData] = useState([]);
+  const [ladderPositions, setLadderPositions] = useState([]);
+
+  // Use Effect
+  useEffect(() => {
+    fetchFootyData();
+  }, [year, round]);
+
+  const updateSeasonChange = (e) => {
+    console.log(e.target.value);
+    setYear(e.target.value);
+  }
+
+  const updateRoundChange = (e) => {
+    console.log(e.target.value);
+    setRound(e.target.value);
+  }
+
+  async function fetchFootyData() {
+    const roundResponse = await axios.get(`https://api.squiggle.com.au/?q=games&year=${year}&round=${round}`);
+    console.log(roundResponse.data.games);
+    setMatchData(roundResponse.data.games);
+
+    const ladderResponse = await axios.get(`https://api.squiggle.com.au/?q=standings&year=${year}&round=${round}`);
+    console.log(ladderResponse.data.standings);
+    setLadderPositions(ladderResponse.data.standings);
+  }
+
   return (
-    <div className="App">
-      <h1>Welcome to FootyBytes</h1>
-    </div>
+    <Container className="App">
+      <Navbar year={year} round={round} refreshSeasonData={updateSeasonChange} refreshRoundData={updateRoundChange} />
+
+      <div id="game-result-card">
+        <Row xs={1} md={2} className="mt-2 mb-4 g-4">
+          {matchData.map(match => (
+            <Col key={match.id}>
+              <Card>
+                <Card.Body>
+                  <Card.Title><strong>{match.date}</strong></Card.Title>
+                  <Card.Text className="mt-4">
+                    <p><strong>{match.hteam}</strong></p>
+                    <p><strong>{match.hgoals}.{match.hbehinds}.{match.hscore}</strong></p>
+                    <p><strong>Vs</strong></p>
+                    <p><strong>{match.ateam}</strong></p>
+                    <p><strong>{match.agoals}.{match.abehinds}.{match.ascore}</strong></p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+
+      <div id="ladder">
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Pos</th>
+              <th>Team</th>
+              <th>Pts</th>
+              <th>P</th>
+              <th>Won</th>
+              <th>Lost</th>
+              <th>Drawn</th>
+              <th>PF</th>
+              <th>PA</th>
+              <th>%</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ladderPositions.map(standings => (
+              <tr key={standings.rank}>
+                <td>{standings.rank}</td>
+                <td><strong>{standings.name}</strong></td>
+                <td>{standings.pts}</td>
+                <td>{standings.played}</td>
+                <td>{standings.wins}</td>
+                <td>{standings.losses}</td>
+                <td>{standings.draws}</td>
+                <td>{standings.for}</td>
+                <td>{standings.against}</td>
+                <td>{standings.percentage}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Form from 'react-bootstrap/Form';
+
+function Navbar(props) {
+
+  const { refreshSeasonData, refreshRoundData, year, round } = props;
+
+  return (
+    <div>
+      <h1>Welcome to FootyBytes</h1>
+
+      <Form.Select name="year" id="year" value={year} onChange={refreshSeasonData}>
+        <option value={2021}>2021</option>
+        <option value={2020}>2020</option>
+        <option value={2019}>2019</option>
+        <option value={2018}>2018</option>
+        <option value={2017}>2017</option>
+        <option value={2016}>2016</option>
+        <option value={2015}>2015</option>
+        <option value={2014}>2014</option>
+        <option value={2013}>2013</option>
+        <option value={2012}>2012</option>
+        <option value={2011}>2011</option>
+        <option value={2010}>2010</option>
+        <option value={2009}>2009</option>
+        <option value={2008}>2008</option>
+        <option value={2007}>2007</option>
+        <option value={2006}>2006</option>
+        <option value={2005}>2005</option>
+        <option value={2004}>2004</option>
+        <option value={2003}>2003</option>
+        <option value={2002}>2002</option>
+        <option value={2001}>2001</option>
+        <option value={2000}>2000</option>
+      </Form.Select>
+      <br />
+      <Form.Select name="round" id="round" value={round} onChange={refreshRoundData}>
+        <option value={1}>Round 1</option>
+        <option value={2}>Round 2</option>
+        <option value={3}>Round 3</option>
+        <option value={4}>Round 4</option>
+        <option value={5}>Round 5</option>
+        <option value={6}>Round 6</option>
+        <option value={7}>Round 7</option>
+        <option value={8}>Round 8</option>
+        <option value={9}>Round 9</option>
+        <option value={10}>Round 10</option>
+        <option value={11}>Round 11</option>
+        <option value={12}>Round 12</option>
+        <option value={13}>Round 13</option>
+        <option value={14}>Round 14</option>
+        <option value={15}>Round 15</option>
+        <option value={16}>Round 16</option>
+        <option value={17}>Round 17</option>
+        <option value={18}>Round 18</option>
+        <option value={19}>Round 19</option>
+        <option value={20}>Round 20</option>
+        <option value={21}>Round 21</option>
+        <option value={22}>Round 22</option>
+        <option value={23}>Round 23</option>
+      </Form.Select>
+
+    </div>
+  )
+}
+
+export default Navbar
